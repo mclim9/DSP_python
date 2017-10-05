d86c5ad00b62e10050b10ed21677d8241fde1312
mclim <martin.lim@rsa.rohde-schwarz.com>
2017-10-04 18:07:57 -0700

    Initial Files

---
+++ DSP_python/CWGen.py
@@ -0,0 +1,100 @@
+######################################################################
+#### Rohde & Schwarz Single tone generation
+######################################################################
+####
+#### This program will test various baseband analysis input types.  
+####
+#### Modified by Martin C Lim
+#### Revision: V0.1
+#### Date    : 2017.10.04
+####
+#### 171004 MCL Created first version
+####
+#######################################
+#### User Input
+#######################################
+maxAmpl = 1.0;                #clipping value
+OverSamp = 30;                #Oversampling
+FC1 = 1.5e6;                  #Tone1,Hz
+FC2 = 2.5e6;                  #Tone2,Hz
+NumPeriods = 100; 
+
+#######################################
+#### Code Begin
+#######################################
+import matplotlib.pyplot as plt
+import numpy as np
+
+def Gen1Tone():
+   Fs = OverSamp*FC1;               #Sampling Frequency
+   StopTime = NumPeriods/FC1;       #Waveforms
+   dt = 1/Fs;                       #seconds per sample
+   t = np.arange(0,StopTime-dt,dt); #create time array
+   I_Ch = np.cos(2*np.pi*FC1*t);
+   Q_Ch = np.sin(2*np.pi*FC1*t);
+
+   #Q_Ch(Q_Ch>maxVal) = maxVal;      #clip Q_Ch maxVal
+   #Q_Ch(Q_Ch<-maxVal) = -maxVal;    #clip Q_Ch minVal
+   #I_Ch(I_Ch>maxVal) = maxVal;      #clip Q_Ch maxVal
+   #I_Ch(I_Ch<-maxVal) = -maxVal;    #clip Q_Ch minVal
+
+   fot = open("CreateWv.env", 'w')
+   fot.write("#################################\n")
+   fot.write("### CWGen Waveform\n")
+   fot.write("###    Oversampling: %d\n"%OverSamp)
+   fot.write("###    Tone Freq   : %.3f\n"%FC1)
+   fot.write("###    Cycles      : %d\n"%NumPeriods)
+   fot.write("###\n")
+   fot.write("#CWGen.py:%dx Oversampled %.0fHz CW\n"%(OverSamp,FC1))
+   fot.write("%f\n"%Fs)
+   for i in range(0,len(I_Ch)):
+      fot.write("%f,%f\n"%(I_Ch[i],Q_Ch[i]))
+   fot.close()
+   print("CWGen: %.0fHz tone generated"%FC1)
+
+def Gen2Tone():
+   Fs = OverSamp*FC1;               #Sampling Frequency
+   StopTime = NumPeriods/FC1;       #Waveforms
+   dt = 1/Fs;                       #seconds per sample
+   t = np.arange(0,StopTime-dt,dt); #create time array
+   I1_Ch = 0.5 * np.cos(2*np.pi*FC1*t);
+   Q1_Ch = 0.5 * np.sin(2*np.pi*FC1*t);
+   I2_Ch = 0.5 * np.cos(2*np.pi*FC2*t);
+   Q2_Ch = 0.5 * np.sin(2*np.pi*FC2*t);
+   I_Ch = I1_Ch + I2_Ch
+   Q_Ch = Q1_Ch + Q2_Ch
+
+   fot = open("CreateWv.env", 'w')
+   fot.write("#################################\n")
+   fot.write("### CWGen Waveform\n")
+   fot.write("###    Oversampling: %d\n"%OverSamp)
+   fot.write("###    Tone1 Freq  : %.3f\n"%FC1)
+   fot.write("###    Tone2 Freq  : %.3f\n"%FC2)
+   fot.write("###    Cycles      : %d\n"%NumPeriods)
+   fot.write("###\n")
+   fot.write("#CWGen.py:%dx Oversampled %.0fHz & %.0fHz CW\n"%(OverSamp,FC1,FC2))
+   fot.write("%f\n"%Fs)
+   for i in range(0,len(I_Ch)):
+      fot.write("%f,%f\n"%(I_Ch[i],Q_Ch[i]))
+   fot.close()
+   print("CWGen: %.0fHz %.0fHz tones generated"%(FC1,FC2))
+   plotData(t, I_Ch, Q_Ch)
+
+def plotData(t, I_Ch, Q_Ch):
+   #######################################
+   #### Plot Data
+   #######################################
+   plt.plot(t, I_Ch, t, Q_Ch)
+   plt.xlabel('time,sec')
+   plt.ylabel('magnitude')
+   plt.title('plot')
+   plt.grid(True)
+   #plt.savefig("test.png")
+   plt.show()
+
+#####################################################################
+### Run if Main
+#####################################################################
+if __name__ == "__main__":
+   Gen2Tone()
+   #plotData(t, I_Ch, Q_Ch)
---
+++ DSP_python/CreateWv.py
@@ -0,0 +1,130 @@
+#*****************************************************************
+# Title: R&S Waveform creation
+#
+# Reference: 
+#       AppNote 1GP62 Sec4 pg 15
+#
+# Instr:
+#   SGT-Modulated VSG
+#   SMW-Modulated VSG
+#
+# Input file:
+#    Header
+#      Comment line begins with "#"
+#      Script will ignore initial lines w/ #
+#      Last Comment line will be used in *.wv
+#   First non comment line is the sampling rate in Hz
+#   Subsequent lines are IQ data
+#      Format: I,Q
+#      Values: Sqrt(I^2 + Q^2) < 1
+#      Exampl: 0.34345435,-1.398283
+#
+#   Sample File:
+#      # This file is ready for convertion
+#      # Waveform created by Rohde & Schwarz
+#      # Please put *.wv comment here
+#      15360000
+#      -0.1800899164,-0.0500845546
+#      -0.0804992785,0.1618766459
+#      -0.0390753583,0.1214229520
+#      0.0361018054,0.1222110829
+#      0.1565244489,-0.0252769231
+#      ...
+#      0.0236197484,-0.1661472281
+#
+#*****************************************************************
+# User Input Settings
+#*****************************************************************
+WaveRead = "CreateWv.env"
+# Comment = ""     #Value in File
+# Clock = 51200000        #Value in File
+
+#*****************************************************************
+# Code Start
+#*****************************************************************
+import math
+import time
+import struct
+import numpy as np
+
+WaveWrit = WaveRead.split(".")[0] + ".wv"
+print WaveWrit
+fin = open(WaveRead, 'r')
+fot = open(WaveWrit, 'wb')
+date = time.strftime("%Y-%m-%d;%H:%M:%S")
+
+#*****************************************************************
+# File Read
+#*****************************************************************
+prevread=" "
+while 1:                                     #Comment Header Parse
+   currread  = fin.readline().strip()
+   if currread[:1] != "#":                   #If lines doesn't have "#"
+      clock = currread                       #This line has clock
+      comment = prevread[1:]                 #Previous line has Comment
+      break
+   prevread = currread
+
+IQArry = np.array([])
+IQArry = [line.strip().split(',') for line in fin]
+IQArry = np.asfarray(IQArry,float)
+samples = len(IQArry)
+fin.close()                                  #Close Input File
+
+#*****************************************************************
+# Calculate RMS
+#*****************************************************************
+RMS = 0
+MAX = 0
+for IQ in IQArry:
+   SQR = pow(IQ[0],2)+pow(IQ[1],2)
+   RMS += SQR
+   if SQR > MAX:
+      MAX = SQR
+
+RMS = 10*math.log10(samples/RMS)
+MAX = 10*math.log10(1/MAX)
+   
+print("Comment:%s"%comment)
+print("ClockRt:%s"%clock)
+print("Samples:%d"%samples)
+print("RMSValu:%f"%RMS)
+print("MaxValu:%f"%MAX)
+
+#*****************************************************************
+# File Header Write
+#*****************************************************************
+fot.write("{TYPE: SMU-WV,0}")                #Type: No change needed.
+fot.write("{COMMENT: %s}"%comment)           #Comment
+fot.write("{DATE:%s}"%date)                  #Date:2005-11-25;12:33:51
+fot.write("{CLOCK:%s}"%clock)                #Clock
+fot.write("{CLOCK MARKER: %s}"%clock)        #Clock
+fot.write("{LEVEL OFFS:%.4f,%.4f}"%(RMS,MAX))   #RMS,Peak
+                                                #RMS = 10*log(sum(i^2+q^2)/numsamp)
+                                                #Peak = 10*log(max(i^2+q^2))
+fot.write("{SAMPLES:%d}"%samples)            #NumSamples
+#fot.write("{CONTROL LENGTH:%d}"%samples)    #NumSamples MkrOnly?
+fot.write("{MARKER LIST 1: 0:1;20:0}")       #MarkerList MkrOnly
+fot.write("{MARKER LIST 2: 0:0}")            #MarkerList MkrOnly
+fot.write("{MARKER LIST 3: 0:0}")            #MarkerList MkrOnly
+fot.write("{MARKER LIST 4: 0:0}")            #MarkerList MkrOnly
+fot.write("{WAVEFORM-%d: #"%(4*samples+1))   #Waveform = NumSamples * 4 + 1
+
+#*****************************************************************
+# File Data Write
+#
+# IQData is a 2byte little edian integer
+# IQData = Round(Real * 32768)
+#*****************************************************************
+i=0
+for IQ in IQArry:
+   if (IQ[0] > 1) or (IQ[1] > 1):
+      print("Error IQ > 1: %f, %f"%(IQ[0],IQ[1]))
+   data = round(IQ[0] * 32767)        
+   data = struct.pack('<h',data)             #i:4byte h:2byte H:unsigned2byte
+   fot.write(data)
+   data = round(IQ[1] * 32767)
+   data = struct.pack('<h',data)             #i:4byte h:2byte H:unsigned2byet
+   fot.write(data)
+fot.write("}")
+fot.close()                                  #Close Output File
