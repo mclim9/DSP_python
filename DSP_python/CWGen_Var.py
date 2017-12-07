4203a74181fb7b4fd02049b930d2a2a934bd2198
mclim <martin.lim@rsa.rohde-schwarz.com>
2017-12-07 13:29:04 -0800

    GUI Alpha

---
+++ DSP_python/CWGen_Var.py
@@ -0,0 +1,21 @@
+class CWGen:
+   def __init__(self):
+      self.maxAmpl = 1.0;                #clipping value
+      self.OverSamp = 10;                #Oversampling
+      self.FC1 = 1.5e6;                  #Tone1,Hz
+      self.FC2 = 2.0e6;                  #Tone2,Hz
+      self.NumPeriods = 60               #Number of Periods
+      self.fBeta = 10;                   #Filter Beta
+
+   def __str__(self):
+      OutStr = 'maxAmpl    : %5.2f\n'%self.maxAmpl +\
+               'OverSamp   : %5.2f\n'%self.OverSamp +\
+               'FC1        : %5.2f\n'%self.FC1 +\
+               'FC2        : %5.2f\n'%self.FC2 +\
+               'NumPeriods : %5.2f\n'%self.NumPeriods +\
+               'fBeta      : %5.2f\n'%self.fBeta 
+      return OutStr
+        
+if __name__ == "__main__":
+   Test = CWGen()
+   print(Test)
