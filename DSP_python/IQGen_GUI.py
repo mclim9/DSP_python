dd2a6a01dbe42f299898a031df99adfc02c30cbd
Martin Lim <trabin9@gmail.com>
2020-12-18 11:26:01 -0800

    chg spacing.

--- DSP_python/IQGen_GUI.py
+++ DSP_python/IQGen_GUI.py
@@ -1,10 +1,5 @@
-from __future__ import division      #int div to float
-# *****************************************************************
-# Title: Simple GUI to display DSP concepts
-#
-# Description:
-#
-#
+"""Simple GUI to display DSP concepts"""
+from __future__ import division                     #int div to float
 # *****************************************************************
 # User Input Settings
 # *****************************************************************
@@ -38,7 +33,7 @@ END = Tkinter.END
 import pickle       #to save/load object
 import copy         #copy object
 # from os.path        import split
-from IQGen_2Tone    import IQGen                                    #pylint: disable=E0401
+from IQGen_2Tone    import IQGen                            #pylint: disable=E0401
 CWVar = IQGen()
 
 # *****************************************************************
@@ -57,9 +52,9 @@ def btn_Waveforms():
 
 def btn_WaveCreate():
     try:          #Python 2.7
-        execfile("CreateWv.py")                                     #pylint: disable=E0602
+        execfile("CreateWv.py")                             #pylint: disable=E0602
     except:     #Python 3.7
-        exec(open("./CreateWv3.py").read())                         #pylint: disable=W0122
+        exec(open("./CreateWv3.py").read())                 #pylint: disable=W0122
     fprintf("Waveform File created")
 
 def btn_SaveCond():
@@ -90,8 +85,8 @@ def btn_PlotFFT():
     CWVar.NumPeriods    = float(Entry3.get())
     CWVar.fBeta         = float(Entry4.get())
     CWVar.OverSamp      = float(Entry5.get())
-    # CWVar.GUI_Element   = lstOutpt              #Send GUI window handle
-    # CWVar.GUI_Object    = GUI                   #Send GUI handle
+    # CWVar.GUI_Element   = lstOutpt                        #Send GUI window handle
+    # CWVar.GUI_Object    = GUI                             #Send GUI handle
     # print(type(lstOutpt))
     CWVar.Gen2Tone()
     CWVar.plot_IQ_FFT()
@@ -149,23 +144,23 @@ def dataLoad():
 # Define GUI Widgets
 # *****************************************************************
 CWVar = copy.copy(dataLoad())
-GUI = Tkinter.Tk()                                            #Create GUI object
-GUI.title("CW to FFT View")                                #GUI Title
-Lbl1    = Tkinter.Label(GUI, text="FC1")                  #Create Label
+GUI = Tkinter.Tk()                                          #Create GUI object
+GUI.title("CW to FFT View")                                 #GUI Title
+Lbl1    = Tkinter.Label(GUI, text="FC1")                    #Create Label
 Entry1  = Tkinter.Entry(GUI,bg=ColorBG, fg=ColorFG,insertbackground=ColorCurs) #Create Entry background
-Entry1.insert(END, CWVar.FC1)                             #Default Value
-Lbl2    = Tkinter.Label(GUI, text="FC2")                  #Create Label
+Entry1.insert(END, CWVar.FC1)                               #Default Value
+Lbl2    = Tkinter.Label(GUI, text="FC2")                    #Create Label
 Entry2  = Tkinter.Entry(GUI,bg=ColorBG, fg=ColorFG,insertbackground=ColorCurs) #Entry Background
-Entry2.insert(END, CWVar.FC2)                             #Default Value
-Lbl3    = Tkinter.Label(GUI, text="NumPeriods")         #Create Label
+Entry2.insert(END, CWVar.FC2)                               #Default Value
+Lbl3    = Tkinter.Label(GUI, text="NumPeriods")             #Create Label
 Entry3  = Tkinter.Entry(GUI,bg=ColorBG, fg=ColorFG,insertbackground=ColorCurs) #Entry Background
-Entry3.insert(END, CWVar.NumPeriods)                    #Default Value
-Lbl4    = Tkinter.Label(GUI, text="Filter Beta")        #Create Label
+Entry3.insert(END, CWVar.NumPeriods)                        #Default Value
+Lbl4    = Tkinter.Label(GUI, text="Filter Beta")            #Create Label
 Entry4  = Tkinter.Entry(GUI,bg=ColorBG, fg=ColorFG,insertbackground=ColorCurs) #Entry Background
-Entry4.insert(END, CWVar.fBeta)                          #Default Value
-Lbl5    = Tkinter.Label(GUI, text="OverSamp=Fs/FC1")  #Create Label
+Entry4.insert(END, CWVar.fBeta)                             #Default Value
+Lbl5    = Tkinter.Label(GUI, text="OverSamp=Fs/FC1")        #Create Label
 Entry5  = Tkinter.Entry(GUI,bg=ColorBG, fg=ColorFG,insertbackground=ColorCurs) #Entry Background
-Entry5.insert(END, CWVar.OverSamp)                      #Default Value
+Entry5.insert(END, CWVar.OverSamp)                          #Default Value
 
 btnWaveF = Tkinter.Button(GUI, width=btnWid, text = "Select *.WV", command = btn_Waveforms)
 btnWaveC = Tkinter.Button(GUI, width=btnWid, text = "Gen *.wv", command = btn_WaveCreate)
@@ -213,19 +208,19 @@ srlOutpt.grid(column=maxCol,row=btnRow-1, sticky=(Tkinter.W,Tkinter.N,Tkinter.S)
 menu = Tkinter.Menu(GUI)                                #create dropdown in GUI
 GUI.config(menu=menu)
 
-fileMenu = Tkinter.Menu(menu)                         #create dropdown in menu
+fileMenu = Tkinter.Menu(menu)                           #create dropdown in menu
 fileMenu.add_command(label="Open",command=menu_Open)
 fileMenu.add_command(label="Save",command=menu_Save)
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit",command=menu_Exit)
 
-editMenu = Tkinter.Menu(menu)                         #create dropdown in menu
+editMenu = Tkinter.Menu(menu)                           #create dropdown in menu
 editMenu.add_command(label="Edit",command=menu_Open)
 
-menu.add_cascade(label="File",menu=fileMenu)     #add dropdown menu
-menu.add_cascade(label="Edit",menu=editMenu)     #add dropdown menu
+menu.add_cascade(label="File",menu=fileMenu)            #add dropdown menu
+menu.add_cascade(label="Edit",menu=editMenu)            #add dropdown menu
 
 #*****************************************************************
 # Start Program
 # *****************************************************************
-GUI.mainloop()         #Display window
+GUI.mainloop()                                          #Display window
