d19f0431b25c2de6f63b373c86589453205d5d3a
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2021-12-10 12:50:56 -0800

    updated formatting style

--- src/CreateWv.py
+++ src/CreateWv.py
@@ -1,4 +1,4 @@
-###############################################################################
+# ##############################################################################
 # Title     : R&S Waveform creation
 # Reference : AppNote 1GP62 Sec4 pg 15
 # Input file:
@@ -25,16 +25,16 @@
 #        ...
 #        0.0236197484,-0.1661472281
 #
-###############################################################################
+# ##############################################################################
 # User Input Settings
-###############################################################################
+# ##############################################################################
 WaveRead    = "CreateWv.env"
-Comment     = ""                                        #Read from input file
-Clock       = 983040000                                 #Read from input file
+Comment     = ""                                        # Read from input file
+Clock       = 983040000                                 # Read from input file
 
-###############################################################################
+# ##############################################################################
 # Code Start
-###############################################################################
+# ##############################################################################
 import math
 import time
 import struct
@@ -49,20 +49,20 @@ date = time.strftime("%Y-%m-%d;%H:%M:%S")
 ###############################################################################
 # File Read
 ###############################################################################
-prevread=" "
-while 1:                                            #Comment Header Parse
+prevread = " "
+while 1:                                            # Comment Header Parse
     currread  = fin.readline().strip()
-    if currread[:1] != "#":                         #If lines doesn't have "#"
-        clock = currread                            #This line has clock
-        comment = prevread[1:]                      #Previous line has Comment
+    if currread[:1] != "#":                         # If lines doesn't have "#"
+        clock = currread                            # This line has clock
+        comment = prevread[1:]                      # Previous line has Comment
         break
     prevread = currread
 
 IQArry = np.array([])
 IQArry = [line.strip().split(',') for line in fin]
-IQArry = np.asfarray(IQArry,float)
+IQArry = np.asfarray(IQArry, float)
 samples = len(IQArry)
-fin.close()                                         #Close Input File
+fin.close()                                         # Close Input File
 
 ###############################################################################
 # Calculate RMS
@@ -70,13 +70,13 @@ fin.close()                                         #Close Input File
 RMS = 0
 MAX = 0
 for IQ in IQArry:
-    SQR = pow(IQ[0],2)+pow(IQ[1],2)
+    SQR = pow(IQ[0], 2) + pow(IQ[1], 2)
     RMS += SQR
     if SQR > MAX:
         MAX = SQR
 
-RMS = 10*math.log10(samples/RMS)
-MAX = 10*math.log10(1/MAX)
+RMS = 10 * math.log10(samples / RMS)
+MAX = 10 * math.log10(1 / MAX)
 
 print("  Comment:%s"%comment)
 print("  ClockRt:%s"%clock)
@@ -87,36 +87,36 @@ print("  MaxValu:%f"%MAX)
 ###############################################################################
 # File Header Write
 ###############################################################################
-fot.write("{TYPE: SMU-WV,0}")                           #Type: No change needed.
-fot.write("{COMMENT: %s}"%comment)                      #Comment
-fot.write("{DATE:%s}"%date)                             #Date:2005-11-25;12:33:51
-fot.write("{CLOCK:%s}"%clock)                           #Wavefm Clock
-fot.write("{CLOCK MARKER: %s}"%clock)                   #Marker Clock
-fot.write("{LEVEL OFFS:%.4f,%.4f}"%(RMS,MAX))           #RMS,Peak
-                                                        #RMS : 10*log(sum(i^2+q^2)/numsamp)
-                                                        #Peak: 10*log(max(i^2+q^2))
-fot.write("{SAMPLES:%d}"%samples)                       #NumSamples
-#fot.write("{CONTROL LENGTH:%d}"%samples)               #NumSamples MkrOnly?
-fot.write("{MARKER LIST 1: 0:1;20:0}")                  #MarkerList MkrOnly
-fot.write("{MARKER LIST 2: 0:0}")                       #MarkerList MkrOnly
-fot.write("{MARKER LIST 3: 0:0}")                       #MarkerList MkrOnly
-fot.write("{MARKER LIST 4: 0:0}")                       #MarkerList MkrOnly
-fot.write("{WAVEFORM-%d: #"%(4*samples+1))              #Waveform = NumSamples * 4 + 1
+fot.write("{TYPE: SMU-WV,0}")                           # Type: No change needed.
+fot.write("{COMMENT: %s}"%comment)                      # Comment
+fot.write("{DATE:%s}"%date)                             # Date:2005-11-25;12:33:51
+fot.write("{CLOCK:%s}"%clock)                           # Wavefm Clock
+fot.write("{CLOCK MARKER: %s}"%clock)                   # Marker Clock
+fot.write("{LEVEL OFFS:%.4f,%.4f}"%(RMS, MAX))          # RMS,Peak
+                                                        # RMS : 10*log(sum(i^2+q^2)/numsamp)
+                                                        # Peak: 10*log(max(i^2+q^2))
+fot.write("{SAMPLES:%d}"%samples)                       # NumSamples
+# fot.write("{CONTROL LENGTH:%d}"%samples)              # NumSamples MkrOnly?
+fot.write("{MARKER LIST 1: 0:1;20:0}")                  # MarkerList MkrOnly
+fot.write("{MARKER LIST 2: 0:0}")                       # MarkerList MkrOnly
+fot.write("{MARKER LIST 3: 0:0}")                       # MarkerList MkrOnly
+fot.write("{MARKER LIST 4: 0:0}")                       # MarkerList MkrOnly
+fot.write("{WAVEFORM-%d: #"%(4 * samples + 1))          # Waveform = NumSamples * 4 + 1
 
-###############################################################################
+# ##############################################################################
 # File Data Write
 # IQData is a 2byte little edian integer
 # IQData = Round(Real * 32768)
-###############################################################################
-i=0
+# #############################################################################
+i = 0
 for IQ in IQArry:
     if (IQ[0] > 1) or (IQ[1] > 1):
-        print("Error IQ > 1: %f, %f"%(IQ[0],IQ[1]))
+        print("Error IQ > 1: %f, %f"%(IQ[0], IQ[1]))
     data = round(IQ[0] * 32767)
-    data = struct.pack('<h',data)                       #i:4byte h:2byte H:unsigned2byte
+    data = struct.pack('<h', data)                      # i:4byte h:2byte H:unsigned2byte
     fot.write(data)
     data = round(IQ[1] * 32767)
-    data = struct.pack('<h',data)                       #i:4byte h:2byte H:unsigned2byet
+    data = struct.pack('<h', data)                      # i:4byte h:2byte H:unsigned2byet
     fot.write(data)
 fot.write("}")
-fot.close()                                             #Close Output File
+fot.close()                                             # Close Output File
--- src/CreateWv3.py
+++ src/CreateWv3.py
@@ -1,9 +1,8 @@
-###############################################################################
+# ##############################################################################
 # Title     : R&S Waveform creation
 # Reference : AppNote 1GP62 Sec4 pg 15
 # Input file:
 #     Header
-#        Comment line begins with "#"
 #        Script will ignore initial lines w/ #
 #        Last Comment line will be used in *.wv
 #    First non comment line is the sampling rate in Hz
@@ -14,7 +13,6 @@
 #
 #    Sample File:
 #        # This file is ready for convertion
-#        # Waveform created by Rohde & Schwarz
 #        # Please put *.wv comment here
 #        15360000
 #        -0.1800899164,-0.0500845546
@@ -24,22 +22,21 @@
 #        0.1565244489,-0.0252769231
 #        ...
 #        0.0236197484,-0.1661472281
-#
-###############################################################################
-# User Input Settings
-###############################################################################
-WaveRead    = "CreateWv.env"
-Comment     = ""                                        #Read from input file
-Clock       = 983040000                                 #Read from input file
-
-###############################################################################
-# Code Start
-###############################################################################
 import math
 import time
 import struct
 import numpy as np
 
+# ##############################################################################
+# User Input Settings
+# ##############################################################################
+WaveRead    = "CreateWv.env"
+Comment     = ""
+Clock       = 983040000
+
+# ##############################################################################
+# Code Start
+# ##############################################################################
 WaveWrit = WaveRead.split(".")[0] + ".wv"
 print("CreateWv.py:" + WaveWrit)
 fin = open(WaveRead, 'r')
@@ -49,20 +46,20 @@ date = time.strftime("%Y-%m-%d;%H:%M:%S")
 ###############################################################################
 # File Read
 ###############################################################################
-prevread=" "
-while 1:                                            #Comment Header Parse
+prevread = " "
+while 1:                                            # Comment Header Parse
     currread  = fin.readline().strip()
-    if currread[:1] != "#":                         #If lines doesn't have "#"
-        clock = currread                            #This line has clock
-        comment = prevread[1:]                      #Previous line has Comment
+    if currread[:1] != "#":                         # If lines doesn't have "#"
+        clock = currread                            # This line has clock
+        comment = prevread[1:]                      # Previous line has Comment
         break
     prevread = currread
 
 IQArry = np.array([])
 IQArry = [line.strip().split(',') for line in fin]
-IQArry = np.asfarray(IQArry,float)
+IQArry = np.asfarray(IQArry, float)
 samples = len(IQArry)
-fin.close()                                         #Close Input File
+fin.close()                                         # Close Input File
 
 ###############################################################################
 # Calculate RMS
@@ -70,53 +67,51 @@ fin.close()                                         #Close Input File
 RMS = 0
 MAX = 0
 for IQ in IQArry:
-    SQR = pow(IQ[0],2)+pow(IQ[1],2)
+    SQR = pow(IQ[0], 2) + pow(IQ[1], 2)
     RMS += SQR
     if SQR > MAX:
         MAX = SQR
 
-RMS = 10*math.log10(samples/RMS)
-MAX = 10*math.log10(1/MAX)
+RMS = 10 * math.log10(samples / RMS)
+MAX = 10 * math.log10(1 / MAX)
 
-print("  Comment:%s"%comment)
-print("  ClockRt:%s"%clock)
-print("  Samples:%d"%samples)
-print("  RMSValu:%f"%RMS)
-print("  MaxValu:%f"%MAX)
+print(f"  Comment:{comment}")
+print(f"  ClockRt:{clock}")
+print(f"  Samples:{samples}")
+print(f"  RMSValu:{RMS:.6f}")
+print(f"  MaxValu:{MAX:.6f}")
 
 ###############################################################################
 # File Header Write
 ###############################################################################
-fot.write("{TYPE: SMU-WV,0}".encode())                  #Type: No change needed.
-fot.write(("{COMMENT: %s}"%comment).encode())           #Comment
-fot.write(("{DATE:%s}"%date).encode())                  #Date:2005-11-25;12:33:51
-fot.write(("{CLOCK:%s}"%clock).encode())                #Wavefm Clock
-fot.write(("{CLOCK MARKER: %s}"%clock).encode())        #Marker Clock
-fot.write(("{LEVEL OFFS:%.4f,%.4f}"%(RMS,MAX)).encode())#RMS,Peak
-                                                        #RMS : 10*log(sum(i^2+q^2)/numsamp)
-                                                        #Peak: 10*log(max(i^2+q^2))
-fot.write(("{SAMPLES:%d}"%samples).encode())            #NumSamples
-#fot.write(("{CONTROL LENGTH:%d}"%samples).encode())    #NumSamples MkrOnly?
-fot.write("{MARKER LIST 1: 0:1;20:0}".encode())         #MkrList MkrOnly
-fot.write("{MARKER LIST 2: 0:0}".encode())              #MkrList MkrOnly
-fot.write("{MARKER LIST 3: 0:0}".encode())              #MkrList MkrOnly
-fot.write("{MARKER LIST 4: 0:0}".encode())              #MkrList MkrOnly
-fot.write(("{WAVEFORM-%d: #"%(4*samples+1)).encode())   #Waveform = NumSamples * 4 + 1
+fot.write("{TYPE: SMU-WV,0}".encode())                  # Type: No change needed.
+fot.write(f"{{COMMENT: {comment}}}".encode())           # Comment
+fot.write(f"{{DATE:{date}}}".encode())                  # Date:2005-11-25;12:33:51
+fot.write(f"{{CLOCK:{clock}}}".encode())                # Wavefm Clock
+fot.write(f"{{CLOCK MARKER: {clock}}}".encode())        # Marker Clock
+fot.write(f"{{LEVEL OFFS:{RMS:.4f},{MAX:.4f}}}".encode())# RMS,Peak
+fot.write(f"{{SAMPLES:{samples:d}}}".encode())          # NumSamples
+fot.write("{MARKER LIST 1: 0:1;20:0}".encode())         # MkrList MkrOnly
+fot.write("{MARKER LIST 2: 0:0}".encode())              # MkrList MkrOnly
+fot.write("{MARKER LIST 3: 0:0}".encode())              # MkrList MkrOnly
+fot.write("{MARKER LIST 4: 0:0}".encode())              # MkrList MkrOnly
+numBytes = 4 * samples + 1
+fot.write((f"{{WAVEFORM-{numBytes:d}: #").encode())     # Waveform = NumSamples * 4 + 1
 
-###############################################################################
+# ##############################################################################
 # File Data Write
 # IQData is a 2byte little edian integer
 # IQData = Round(Real * 32768)
-###############################################################################
-i=0
+# ##############################################################################
+i = 0
 for IQ in IQArry:
     if (IQ[0] > 1) or (IQ[1] > 1):
-        print("Error IQ > 1: %f, %f"%(IQ[0],IQ[1]))
+        print(f"Error IQ > 1: {IQ[0]}, {IQ[1]}")
     data = int(round(IQ[0] * 32767))
-    data = struct.pack('<h',data)                       #i:4byte h:2byte H:unsigned2byte
+    data = struct.pack('<h', data)                      # i:4byte h:2byte H:unsigned2byte
     fot.write(data)
     data = int(round(IQ[1] * 32767))
-    data = struct.pack('<h',data)                       #i:4byte h:2byte H:unsigned2byet
+    data = struct.pack('<h', data)                      # i:4byte h:2byte H:unsigned2byet
     fot.write(data)
 fot.write("}".encode())
-fot.close()                                             #Close Output File
+fot.close()                                             # Close Output File
